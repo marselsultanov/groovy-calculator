a5b92a1d01188d2fad8582159475d64c7afbb9d0
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-04-17 12:06:30 +0300

    Update GroovyCalculator.groovy

--- src/main/groovy/GroovyCalculator.groovy
+++ src/main/groovy/GroovyCalculator.groovy
@@ -8,7 +8,7 @@ def result = {
 //Call closure, with class which support string expression with unlimited arguments, brackets and specific operators
 result(evalresult)
 
-//Using operator vverloading. In this example we invert plus to minus
+//Using operator overloading. In this example we invert plus to minus
 class Number {
     double x
     Number(double x) {
@@ -22,5 +22,5 @@ class Number {
 def x = evalresult as double
 //Plus the number from itself. It should by 0, becouse of operator overloading
 def example = new Number(x) + new Number(x).x
-print "Result overloading is "
+print "Result operator overloading is "
 println example.x as Integer
